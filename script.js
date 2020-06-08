1c768d9c51065ce505d6dff93f683375af011df5
parsecmonkey <60334704+parsecmonkey@users.noreply.github.com>
2020-06-09 08:37:04 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$('.logo-on-off').each(function() {
+
+  let img_off = $(this).attr('src');
+  let img_on = $(this).attr('src').replace('-icon', '-icon-eye');
+
+  $(this).hover(
+    function () {
+      $(this).attr('src', img_on);
+    },
+    function () {
+      $(this).attr('src', img_off);
+    }
+  );
+
+});

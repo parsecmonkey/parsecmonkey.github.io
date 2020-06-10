9de178f41e4497f51797d5e2698df45f7d5a768d
parsecmonkey <60334704+parsecmonkey@users.noreply.github.com>
2020-06-11 01:44:47 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 画像切り替え
 $('.logo-on-off').each(function() {
 
   let img_off = $(this).attr('src');
@@ -12,5 +13,18 @@ $('.logo-on-off').each(function() {
         $(this).attr('src', img_off);
       }
   );
+});
 
+// ページ内リンクジャンプ
+jQuery(function(){
+  var windowWidth = $(window).width();
+  var headerHight = 100; // ヘッダーの高さ
+  jQuery('a[href^=#]').click(function() { // <a href="#〇〇"> をクリックしたとき
+    var speed = 1000; // スクロールのスピード
+    var href= jQuery(this).attr("href");
+    var target = jQuery(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top-headerHight; // 表示位置=ブラウザTOP-ヘッダーの高さ
+    jQuery('body,html').animate({scrollTop:position}, speed, 'swing');
+　  return false;
+  });
 });

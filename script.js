db5a9a7ef81c38d98aa0cb0d52ac7500390ab2f4
parsecmonkey <60334704+parsecmonkey@users.noreply.github.com>
2021-06-14 10:47:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// 画像切り替え
-$('.logo-on-off').each(function() {
-
-  let img_off = $(this).attr('src');
-  let img_on = $(this).attr('src').replace('-icon', '-icon-ton');
-  var i = 0;
-
-  $(this).hover(
-      function () {
-        $(this).attr('src', img_on);
-      },
-      function () {
-        $(this).attr('src', img_off);
-      }
-  );
-});
-
 // ページ内リンクジャンプ
 jQuery(function(){
   var windowWidth = $(window).width();
